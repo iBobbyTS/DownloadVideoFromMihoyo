b9e342c3600d2aca95d2721225958496f806b7c2
iBobbyTS <ibobbyts@gmail.com>
2024-04-07 19:47:35 -0600

    test corsproxy

--- gi.js
+++ gi.js
@@ -1,9 +1,13 @@
+let url = 'https://corsproxy.io?https://content-static.mihoyo.com/content/ysCn/getContentList?pageSize=10000&pageNum=1&channelId=10';
+fetch(url, {cache: "no-store"})
+    .then(response => {
+        alert('成功')
+    });
 function search() {
     let keyword = document.getElementById('keyword-input').value;
-    let url = 'https://content-static.mihoyo.com/content/ysCn/getContentList?pageSize=10000&pageNum=1&channelId=10';
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
+    let url = 'https://corsproxy.io?https://content-static.mihoyo.com/content/ysCn/getContentList?pageSize=10000&pageNum=1&channelId=10';
+    fetch(url, {cache: "no-store"})
+        .then(response => {
+            alert('成功')
         });
 }
